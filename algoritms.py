c7eed324c964e456eca21d1b01c0f0e8e053d2cb
NoSocksForYou <kaappo.raivio@gmail.com>
2018-02-02 23:36:53 +0200

    added one algoritm.

--- algoritms.py
+++ algoritms.py
@@ -1,10 +1,26 @@
-def diversiness(string):
+import re
+
+def diversiness(string, len_gain=0.2):
     used_chars = {}
     for i in string:
         if i not in used_chars:
             used_chars[i] = 1
         else:
             used_chars[i] += 1
-    return len(used_chars) / len(string)
-print(diversiness('Rita sanoi juuri että pitää ottaa funktio laskin mukaan ensi tunnin koetta varten jos omistaa semmosen Rita sanoi juuri että pitää ottaa funktio laskin mukaan ensi tunnin koetta varten jos omistaa semmosen'))
-    
+    return len(used_chars) ** 1.75 / len(string) / 4
+
+def patterns(string):
+    length = len(string) // 5
+    patterns = 0
+    for i in range(len(string)):
+        try:
+            pattern = r'{}'.format(string[i:i + length])
+        except KeyError:
+            break
+        amount_of_patterns = len(re.findall(pattern, string))
+        if amount_of_patterns > 1:
+            patterns += 1
+    return patterns
+
+print(diversiness('asdasdasdasdasdasdasdasdasdasdasd'))
+print(patterns(input()))
